f85f645612fcea69c21edaffde8400dc63d73733
administrator <administrator@MYCOMPUTER>
2022-06-07 17:31:29 +0500

    Add tests.

---
+++ GameWebApiTests/Common/GameContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using GameWebApi.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace GameWebApiTests.Common
+{
+    public class GameContextFactory
+    {
+        public static GameContext Create(bool withInit=false)
+        {
+            var options = new DbContextOptionsBuilder<GameContext>()
+               .UseInMemoryDatabase(Guid.NewGuid().ToString())
+               .Options;
+
+            GameContext dbContext = new GameContext(options);
+            
+            if (withInit)
+                InitialDatabase.InitialData(dbContext);
+
+            return dbContext;
+        }
+
+        public static void Destroy(GameContext dbContext)
+        {
+            dbContext.Database.EnsureDeleted();
+            dbContext.Dispose();
+        }
+    }
+}
---
+++ GameWebApiTests/Common/TestCommandBase.cs
@@ -0,0 +1,20 @@
+﻿using GameWebApi.Database;
+using System;
+
+namespace GameWebApiTests.Common
+{
+    public abstract class TestCommandBase:IDisposable
+    {
+        protected readonly GameContext Context;
+
+        public TestCommandBase()
+        {
+            Context = GameContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            GameContextFactory.Destroy(Context);
+        }
+    }
+}
